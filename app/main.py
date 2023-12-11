4ac7d4e3d4aafd0932a4abf328b6d43d7f2023e8
Tony Johnson <122806493+ajohnson-va@users.noreply.github.com>
2023-12-10 20:03:18 -0600

    implement run & test with placeholder logic

--- app/main.py
+++ app/main.py
@@ -1,11 +1,33 @@
 import click
+import subprocess
 
 
-@click.command()
-def cli():
-    """Example script."""
-    click.echo('Hello World!')
+@click.group()
+def secrel():
+    """SecRel CLI."""
+    pass
+
+@secrel.group()
+def pipeline():
+    """Pipeline operations."""
+    pass
+
+@pipeline.command()
+def run():
+    """Run the pipeline."""
+    subprocess.run(
+        ['gh', 'repo', 'view', 'department-of-veterans-affairs/lighthouse-tornado-secrel-pipeline'],
+        check=True
+    )
+
+@pipeline.command()
+def test():
+    """Test the pipeline."""
+    subprocess.run(
+        ['gh', 'repo', 'view', 'department-of-veterans-affairs/lighthouse-tornado-secrel-pipeline'],
+        check=True
+    )
 
 
 if __name__ == '__main__':
-    cli()
+    secrel()
