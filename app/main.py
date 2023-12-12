c95e87fa30b2d08aba5bc1a0c474cb38404f3ff4
Tony Johnson <122806493+ajohnson-va@users.noreply.github.com>
2023-12-11 20:40:47 -0600

    add --ref option to commands

--- app/main.py
+++ app/main.py
@@ -13,25 +13,38 @@ def pipeline():
     pass
 
 @pipeline.command()
-def run():
+@click.option(
+    '-r', '--ref',
+    default='main',
+    required=False,
+    help='The git ref or branch of the pipeline to run.'
+)
+def run(ref):
     """Run the pipeline."""
     subprocess.run(
         [
             'gh', 'workflow', 'run', 'pipeline.yml',
             '-R', 'department-of-veterans-affairs/lighthouse-tornado-secrel-pipeline',
-            '-r', 'tony-test-branch'
+            '-r', ref
         ],
         check=True
     )
+    # TODO: better handling for non-zero code returns (failures)
 
 @pipeline.command()
-def test():
+@click.option(
+    '-r', '--ref',
+    default='main',
+    required=False,
+    help='The git ref or branch of the pipeline E2E tests to run.'
+)
+def test(ref):
     """Test the pipeline."""
     subprocess.run(
         [
             'gh', 'workflow', 'run', 'e2e.yml',
             '-R', 'department-of-veterans-affairs/lighthouse-tornado-secrel-pipeline',
-            '-r', 'tony-test-branch'
+            '-r', ref
         ],
         check=True
     )
