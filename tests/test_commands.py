307e6cfc64edfa7ccf5bded3c68df700cba5fc55
Tony Johnson <122806493+ajohnson-va@users.noreply.github.com>
2023-12-18 12:56:37 -0600

    remove pytest debug statements

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -11,21 +11,11 @@ TEST_BRANCH = os.getenv("TEST_BRANCH")
 def test_run_command():
     runner = CliRunner()
     result = runner.invoke(secrel, ['pipeline', 'run', '-r', TEST_BRANCH])
-    print(result)
-    print(result.exit_code)
-    print(result.exc_info)
-    print(result.output)
-    print(result.exception)
 
     assert result.exit_code == 0
 
 def test_test_command():
     runner = CliRunner()
     result = runner.invoke(secrel, ['pipeline', 'test', '-r', TEST_BRANCH])
-    print(result)
-    print(result.exit_code)
-    print(result.exc_info)
-    print(result.output)
-    print(result.exception)
 
     assert result.exit_code == 0
